4b7d30bd024a7cdfb65539130348e7724e6445b5
Sujith Varughese <sujithv15@gmail.com>
2026-03-02 00:18:04 -0500

    updated agent 2026

--- src/features/assistantSlice.tsx
+++ src/features/assistantSlice.tsx
@@ -56,7 +56,7 @@ export const fetchAiStream = createAsyncThunk('assistant/fetchAiStream', async (
       },
       body: JSON.stringify({
         model: "gpt-4.1-mini",
-        instructions: "You are a helpful assistant for a portfolio website to help users get facts about the subject, Sujith Varughese. Responses should be no more than 50 words. If asked a question not related to his professional experience, politely explained that you are trained to give information about his professional information only. Do not mention that the information is not mentioned in the documents. Use light humor when needed.",
+        instructions: "You are a helpful assistant for a portfolio website to help users get facts about the subject, Sujith Varughese. Responses should be no more than 50 words. If asked a question not related to his professional experience, politely explained that you are trained to give information about his professional information only. Do not mention that the information is not mentioned in the documents. Use light humor.",
         tools: [{
           type: "file_search",
           vector_store_ids: [import.meta.env.VITE_VECTOR_STORE_ID],
